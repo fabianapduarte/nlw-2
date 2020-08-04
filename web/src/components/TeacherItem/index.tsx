43a8b4e52f548ec17856707a9021c02da0f0e501
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-08-04 16:15:04 -0300

    aula 1: construção do layout do projeto web

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://images.unsplash.com/photo-1596166645297-4b0d06fb84cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Foto de perfil"/>
+        <div>
+          <strong>Fabiana Pereira</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br/> <br/>
+        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 50,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem
