66188ce9ebfef3346ef2a54b183d31a250dc7b7d
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-08-07 22:47:48 -0300

    criação da landing e da page header; configuração da navegação entre as páginas

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import PageHeader from '../../components/PageHeader'
+
+import styles from './styles'
+
+function Favorites() {
+  return (
+    <View>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  )
+}
+
+export default Favorites
