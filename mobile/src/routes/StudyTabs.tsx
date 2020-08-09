294990a8c6c892e4b9ae69cc2312b5b2189794d6
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-08-09 00:07:41 -0300

    conexão com a api; listagem de professores e favoritos

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -44,7 +44,11 @@ function StudyTabs() {
           tabBarLabel: 'Proffys',
           tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-easel" size={size} color={focused ? '#8257e5' : color} />
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
             )
           }
         }}
@@ -56,7 +60,11 @@ function StudyTabs() {
           tabBarLabel: 'Favoritos',
           tabBarIcon: ({ color, size, focused }) => {
             return (
-              <Ionicons name="ios-heart" size={size} color={focused ? '#8257e5' : color} />
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
             )
           }
         }}
